6d3a89d8aa52db02f0f9aba511e9980194096163
anuragkulkarni20 <68266521+anuragkulkarni20@users.noreply.github.com>
2020-07-21 13:59:56 +0530

    Add files via upload

---
+++ day5.20.7.2020/asss2/array.js
@@ -0,0 +1,11 @@
+let inputArray = [];
+let size = 5; //Maximum Array size
+
+for(let i=0; i<size; i++) {
+	
+	//Taking Input from user
+	inputArray[i] = prompt('Enter Element ' + (i+1));
+}
+
+//Print the array in the console.
+console.log(inputArray);
